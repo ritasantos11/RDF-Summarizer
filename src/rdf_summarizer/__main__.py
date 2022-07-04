38bbdfcdcca63193a3eced3977c4531eac2c03d8
André Santos <andrefs@andrefs.com>
2022-07-04 16:11:04 +0100

    wip

--- src/rdf_summarizer/__main__.py
+++ src/rdf_summarizer/__main__.py
@@ -5,7 +5,7 @@ def main():
     if len(sys.argv) > 1:
         file_name = re.split("^(.*)\.(\w+)$", sys.argv[1])[1]
         file_ext = re.split("^(.*)\.(\w+)$", sys.argv[1])[2]
-        summarization(sys.argv[1], "")
+        summarize(sys.argv[1], "")
 
 if __name__ == "__main__":
     main()
