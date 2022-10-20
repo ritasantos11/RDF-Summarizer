ebe1968eced406270abdc8db82c41e4cb63cd45c
ritasantos11 <37849843+ritasantos11@users.noreply.github.com>
2022-10-20 11:52:19 +0100

    Add files via upload

--- src/rdf_summarizer/write_file.py
+++ src/rdf_summarizer/write_file.py
@@ -1,3 +1,23 @@
+import re
+
+def write_ontology(f):
+    """
+        Write ontology for smaller graph
+
+        Parameters:
+        -----------
+            f: file
+    """
+    f.write("@prefix ngs: <https://ww.dcc.fc.up.pt/~up201605706/ngs#\n")
+    f.write("@prefix xsd: <http://www.w3.org/2001/XMLSchema#>\n")
+    f.write("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n\n")
+
+    f.write("ngs:ReifiedTriple  rdfs:subClassOf rdf:Statement ;\n")
+    f.write("                    rdfs:comment \"RDF statement about the reduced triple\" .\n\n")
+    f.write("ngs:num_occurrences rdfs:domain ngs:ReifiedTriple ;\n")
+    f.write("                    rdfs:range  xsd:integer ;\n")
+    f.write("                    rdfs:comment \"Indicates how many originals triples are converted into the same reduced triple\" .\n\n")
+
 
 def write_graph(file_name, results_triples):
     """
@@ -28,17 +48,21 @@ def write_statements(file_name, results_triples):
             results_triples (dict): reduced triples
     """
     f = open("rdf_statements_" + file_name + ".nt", "w")
-    f.write("@prefix ngs: <https://ww.dcc.fc.up.pt/~up201605706/ngs#\n\n")
+    write_ontology(f)
+    
     num = 1
     for sujeito in results_triples:
         for predicado in results_triples[sujeito]:
             for objeto in results_triples[sujeito][predicado]:
-                f.write("ngs:t" + str(num) + " rdf:type rdf:statement\n")
+                f.write("ngs:t" + str(num) + " rdf:type ngs:ReifiedTriple\n")
                 f.write("ngs:t" + str(num) + " rdf:subject ngs:" + str(sujeito) + "\n")
-                f.write(
-                    "ngs:t" + str(num) + " rdf:predicate ngs:" + str(predicado) + "\n"
-                )
-                f.write("ngs:t" + str(num) + " rdf:object ngs:" + str(objeto) + "\n")
+                f.write("ngs:t" + str(num) + " rdf:predicate ngs:" + str(predicado) + "\n")
+
+                if re.match("^xsd:", str(objeto)):
+                    f.write("ngs:t" + str(num) + " rdf:object " + str(objeto) + "\n")
+                else:
+                    f.write("ngs:t" + str(num) + " rdf:object ngs:" + str(objeto) + "\n")
+                
                 f.write(
                     "ngs:t"
                     + str(num)
